08425337f911c03e8ce5304e847ce4a3fda212f5
suhail09s <killer.x22@gmail.com>
2018-04-11 16:50:35 +0300

    login suppport ,list of downloaded and bug fixes
    + login supported.
    +access to fav.
    +access to sub.
    +access to purity=111.
    +find dublicates.
    +better counter for planned to download.
    +fixed and added limit to threads.
    +check list of already downloaded walls.

--- conf.py
+++ conf.py
@@ -1,16 +1,18 @@
-login =dict(login='suhail',password='pass')# not working YET
-threads='5'					# the number of wallpapers downloaded simultaneously,each wallpaper is a separate downloading thread. depends on connection speed, more=faster :D 
-sorting='date_added' 			 #sorting options(random,toplist,relevance,date_added,views,favorites)
+user='suhail22' #login details to access your favorites and NSFW wallpapers
+password='sohul2002'
+mode='search'				# 3 available modes	(subscription,favorites,search) * search is the default mode and it will work without login. subscription and favorites both require login.
+threads='50'				# the number of wallpapers downloaded simultaneously,each wallpaper is a separate downloading thread. depends on connection speed, more=faster :D ,117 is the limit not the sky
+sorting='random' 			 #sorting options(random,toplist,relevance,date_added,views,favorites)
 search='' 	 				#search ,you can leave empty.
 toprange='1y'				# download toplist(if enabled in sorting) for specific period(last day=1d,last 3 days=3d,last week=1w,last month=1M
 							#last 3 months =3M,last 6 months=6M, last year=1y)
 numpages='1'  	 			#number of pages to download(24 in each page).
-startpage='10'				#start downloading from a certain page(none will be downloaded if page does not exist)
-order='asc'				 #order(desc or asc).
-cat='100'				 	#categories (General=100,Anime=010,People=001) you can add them together or enable a single category only
+startpage='1'				#start downloading from a certain page(none will be downloaded if page does not exist)
+order='desc'				 #order(desc or asc).
+cat='111'				 	#categories (General=100,Anime=010,People=001) you can add them together or enable a single category only
 purity='100'				 #purity (SFW=100,Sketchy=010,NSFW=001) *NSFW is not supported.
 resolution='' #specify the resolution(s) needed ,use (,) to specify more resolutions (or leave empty).
-ratio=''						# specify ratio(s) ,use (,) to specify more rations (or leave empty),for example (16x9)
-createfolder='False'				#save wallpapers in a new folder, use True or False - the folder will be renamed as (Wallpapers-*date and time*) 
+ratio='16x9'						# specify ratio(s) ,use (,) to specify more rations (or leave empty),for example (16x9)
+createfolder='True'				#save wallpapers in a new folder, use True or False - the folder will be renamed as (Wallpapers-*date and time*) 
 savedir='Wallpapers/'				#save directory ,also if createfolder is True,it will create a new folder inside savedir ,directories can be relative (wallpapers/) or absolute(d:/wallpapers/) *the last backslash is required.
 							#leave empty to create the folder in the same directory.
--- wallhavenDownloader.py
+++ wallhavenDownloader.py
@@ -10,11 +10,15 @@ q=Queue()
 
 ############################
 # reads a config file attached (conf.py) with the main .py for a castomizable downloader 
+
 try:
     open("conf.py",'r')
     search=str(conf.search).replace(' ','+') # replace space with plus to mkae the url
     dthread=int (conf.threads)
+    us=conf.user
+    ps=conf.password
     cat=conf.cat
+    mode=conf.mode
     purity=conf.purity
     sorting=conf.sorting    
     order=conf.order
@@ -47,10 +51,24 @@ def worker (single):# this thread worker takes each link and try to parse the di
     tagfound=tagfind(parsed,'img')
     downloader(tagfound)
     
-
+def login(uname,pas):
+        
+        url_login='https://alpha.wallhaven.cc/auth/login'
+        getcsrf=c.get(url_login)
+        parsedcsrf =parser(getcsrf)
+        csrf=tagfind(parsedcsrf,'csrf')
+        login_data=dict(csrfmiddlewaretoken=csrf, username=us,password=ps)
+        c.post(url_login,data=login_data)
+        checklogin=str(c.get('https://alpha.wallhaven.cc').content)
+        if (checklogin.find('Welcome back')) == -1: # check if logged in or not, by simply reading the welcome msg.
+            print('Failed to login,Check username and password. \nAll features that require login will be disabled.\n')
+        else:
+            print ('logged in')
+        
+        return csrf
 def pageget (url):
     
-    page=requests.get(url)
+    page=c.get(url)
 
     return page
 
@@ -68,16 +86,24 @@ def tagfind(soup,mode):
         tag1=soup.find (id='wallpaper')
    
         return tag1
-    
+    if mode=='csrf':
+        tag1=soup.find (id='login')
+        tag2=tag1.findAll ('input')
+        csrftag=str(tag2).split('"')[5]
+        return csrftag
+
 def listlinks (tags):
     links=[]
     for tag in tags:
         link=tag.get('href')
-        splink=link.split('/')[-1]
+        splink=link.split('/')
         
-        if str.isdigit(splink):
-            links.append(link)
-            print('added '+link)
+        if (str.isdigit(splink[-1])) and ((splink)[-2] != 'quickFavForm'):
+            if any(splink[-1] in l for l in downloadedwalls):
+               print ('found dublicate')
+            else:
+                links.append(link)
+                print('added '+link)
         else:
             pass
         
@@ -85,11 +111,15 @@ def listlinks (tags):
 
 
 def downloader(link):
+    image=''
+    
     image=link.get('src')
     image='https:'+image 
     
     spname=image.split('/')
     name=spname[-1]
+    
+
     newfolder=''# to avoid creating a new folder if createfolder in conf.py = False
     if createfolder==str('True').upper:
         newfolder='wallpapers-{}/'.format(foldertime)
@@ -98,59 +128,81 @@ def downloader(link):
     try:
         open(savedir+newfolder+'/'+name,'r') # the script will try to check if the wallpaper is already exist. 
         print(name+' is already exist.')
-        
+        exit(1)
         
     except:
         r = requests.get(image, allow_redirects=True)
         open(savedir+newfolder+name, 'wb').write(r.content) # saving the wallpaper.
         print(name+' has been downloaded')
+        with open("list.of.downloaded.wall.txt", "a") as myfile:
+            myfile.write(name+'\n') 
         
                               ##############################  Main script  ####################################
 
-
+downloadedwalls=[]
+try:
+    with open('list.of.downloaded.wall.txt', 'r') as f:
+        downloadedwalls = [str(str(str(line.strip()).replace('.jpg',"")).replace('.png','')).replace('wallhaven-','') for line in f]
+        print(downloadedwalls)
+        print('Downloaded wallpapers list has been read.')
+except:
+    pass
 linksTodownload=[] # a definition of a simple list of all the wallpapers that are planned for downloading (empty at this point).
-
-
-for page in range(startpage,startpage+numpages):
-    
-    url='https://alpha.wallhaven.cc/search?q={}&categories={}&purity={}&resolutions={}&ratios={}&topRange={}&sorting={}&order={}&page={}'\
+ss=0
+with requests.Session() as c:
+    if dthread>118:
+        dthread=117
+        print('excedded the maximum number of threads allowed,117')
+    if ss<1:
+        login(1,1)
+    ss=+1
+    for page in range(startpage,startpage+numpages):
+     if mode=='subscription':
+        url='https://alpha.wallhaven.cc/subscription?purity=111&page={}'.format(str(page))
+     elif mode=='search':
+        url='https://alpha.wallhaven.cc/search?q={}&={}&purity={}&resolutions={}&ratios={}&topRange={}&sorting={}&order={}&page={}'\
             .format(search,cat,purity,resolution,ratio,toprange,sorting,order,str(page))  #constructing the link .
-    print(url)
-    page=pageget(url)
-    parsed=parser(page)
-    tagfound=tagfind(parsed,'catalog')
-    tolist=listlinks(tagfound)
+     elif mode=='favorites':
+         url='https://alpha.wallhaven.cc/favorites?purity={}&page={}'.format(purity,str(page))
+     print(url)
+     page=pageget(url)
+     parsed=parser(page)
+     tagfound=tagfind(parsed,'catalog')
+     tolist=listlinks(tagfound)
 
     
-    for link in tolist: # simply making a list of all the links to are planned for downloading.
-        linksTodownload.append(link)
-if len(linksTodownload)==0: # in case there aren't any wallpapers to be downloaded(empty page for example).
-    print('nothing to download')
-    exit(0)
-    
-print('getting ready to download :'+str(len(linksTodownload))+' wallpapers')
+     for link in tolist: # simply making a list of all the links to are planned for downloading.
+         if any(link in l for l in linksTodownload):
+             continue
+         else:
+            linksTodownload.append(link)
+     if len(linksTodownload)==0: # in case there aren't any wallpapers to be downloaded(empty page for example).
+                print('nothing to download')
        
+    
+    print('getting ready to download :'+str(len(linksTodownload))+' wallpapers')
+   
 
-print('downloading...')
-start=time.time()
-for single in range(dthread):# Starting a thread that processes each link.
-        try:
-            
+    print('downloading...')
+    start=time.time()
+    for single in range(dthread):# Starting a thread that processes each link.
+       try:
+             
              t=threading.Thread(target=threader)
              t.daemon=True
              t.start()
-             
+             time.sleep(0.005)
             
-        except:
-            print('invalid URL ')
-for link in linksTodownload: #Queue manger/builder - adds all links that are planned for downloading in a queue.
+       except:
+                print('invalid URL ')
+    for link in linksTodownload: #Queue manger/builder - adds all links that are planned for downloading in a queue.
     
-    q.put(link)
+            q.put(link)
 
-print('#'*25)
+    print('#'*25)
 
-q.join()
-print('\n \n \n ######## Done downloading, Enjoy :D ########')
-print ("Downloading time was :",time.time()-start)
+    q.join()
+    print('\n \n \n ######## Done downloading, Enjoy :D ########')
+    print ("Downloading time was :",time.time()-start)
 
 
